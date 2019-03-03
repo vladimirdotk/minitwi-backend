ad866474e65dd2b3b0ae766c9b02562d988090c8
Vladimir Kivernik <vladimir@kivernik.ru>
2019-03-04 01:55:44 +0300

    Added users create

--- minitwi/tweets/serializers.py
+++ minitwi/tweets/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
 
 from .models import Tweet
 
@@ -13,9 +14,21 @@ class TweetSerializer(serializers.ModelSerializer):
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
+    email = serializers.EmailField(
+            required=True,
+            validators=[UniqueValidator(queryset=User.objects.all())])
+    username = serializers.CharField(
+            validators=[UniqueValidator(queryset=User.objects.all())])
+    password = serializers.CharField(min_length=8, write_only=True)
     tweets = serializers.HyperlinkedRelatedField(
         many=True, view_name='tweet-detail', read_only=True)
 
+    def create(self, validated_data):
+        return User.objects.create_user(
+            validated_data['username'],
+            validated_data['email'],
+            validated_data['password'])
+
     class Meta:
         model = User
-        fields = ('id', 'url', 'username', 'tweets')
+        fields = ('id', 'url', 'username', 'email', 'password', 'tweets')
